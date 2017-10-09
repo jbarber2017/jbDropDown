dd9fe7d407770a9a1220b6ecca2b97a0cb802a5a
JONATHAN-LAPTOP\jonat <jbarber76@gmail.com>
2017-10-08 20:04:59 -0400

    more files

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,3 +1,47 @@
 export class Utils {
-    
+
+    static exists(value: any): boolean {
+        if (value === null || value === undefined || value === '') {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static missing(value: any): boolean {
+        return !this.exists(value);
+    }
+
+    static iterateObject(object: any, callback: (key: string, value: any) => void) {
+        if (this.missing(object)) {
+            return;
+        }
+        let keys = Object.keys(object);
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            let value = object[key];
+            callback(key, value);
+        }
+    }
+
+    static mapObject<TResult>(object: any, callback: (item: any) => TResult) {
+        let result: TResult[] = [];
+        Utils.iterateObject(object, (key: string, value: any) => {
+            result.push(callback(value));
+        });
+        return result;
+    }
+
+    static forEachSnapshotFirst(list: any[], callback: (item: any)=>void ): void {
+        if (list) {
+            let arrayCopy = list.slice(0);
+            arrayCopy.forEach(callback);
+        }
+    }
+
+    static removeFromArray<T>(array: T[], object: T) {
+        if (array.indexOf(object) >= 0) {
+            array.splice(array.indexOf(object), 1);
+        }
+    }
 }
